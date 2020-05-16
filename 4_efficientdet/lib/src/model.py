9ee79503ea59effa6d16cf171cdcaabb6cf26340
ushur <sasha2k1@gmail.com>
2020-05-16 12:57:57 +0300

    Update model.py

--- 4_efficientdet/lib/src/model.py
+++ 4_efficientdet/lib/src/model.py
@@ -175,7 +175,10 @@ class Classifier(nn.Module):
 class EfficientNet(nn.Module):
     def __init__(self, ):
         super(EfficientNet, self).__init__()
-        model = EffNet.from_pretrained('efficientnet-b0')
+        
+#         model = EffNet.from_pretrained('efficientnet-b0')
+        model = EffNet.from_pretrained('efficientnet-b7')
+        
         del model._conv_head
         del model._bn1
         del model._avg_pooling
