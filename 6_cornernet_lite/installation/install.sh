0044edfaf4b5c99f3cae42f0e5bce05d9c08be00
ushur <sasha2k1@gmail.com>
2020-05-17 12:21:55 +0300

    Удаление лишнего

--- 6_cornernet_lite/installation/install.sh
+++
@@ -1,5 +0,0 @@
-cat requirements.txt | xargs -n 1 -L 1 pip install
-
-cd ../lib/core/models/py_utils/_cpools/ && pip install -e .
-
-cd ../../../external && make
--- 8_pytorch_rfbnet/installation/install.sh
+++
@@ -1,3 +0,0 @@
-cat requirements.txt | xargs -n 1 -L 1 pip install
-
-cd ../lib/ && chmod +x make.sh && ./make.sh
